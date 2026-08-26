5783df29b7e538aad5ac78bf4cbd7fc85ec69ebe
XiaoGao-GGB <3393074626@qq.com>
2026-08-26 16:37:48 +0800

    PWA update: sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 /* 音宝轩宝 情侣小网站 · 离线缓存（PWA） */
-const CACHE = 'love-site-v17';
+const CACHE = 'love-site-v18';
 const CORE = [
   './',
   './index.html',
-  './css/style.css?v=17',
-  './js/config.js?v=17',
-  './js/store.js?v=17',
-  './js/app.js?v=17'
+  './css/style.css?v=18',
+  './js/config.js?v=18',
+  './js/store.js?v=18',
+  './js/app.js?v=18'
 ];
 
 // 安装：预缓存核心文件
